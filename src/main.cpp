33b460810ee590ae63a26d0592aba59bed941bbb
simonkdev <simonkorten09@gmail.com>
2026-08-03 23:18:29 +0200

    added second button with drawing stub to test an external tool for UI creation

--- src/main.cpp
+++ src/main.cpp
@@ -10,8 +10,12 @@
 #include "clock.h"
 
 const int BUTTON_PIN = 44; // the number of the pushbutton pin
+const int BUTTON_PIN_2 = 43;
 int lastState = HIGH; // the previous state from the input pin
 int currentState;
+
+int lastState2 = HIGH;
+int currentState2;
 
 Weather weather;
 
@@ -20,15 +24,18 @@ void setup()
     Serial.begin(115200);
     while(!Serial){};
     pinMode(BUTTON_PIN, INPUT_PULLUP);
+    pinMode(BUTTON_PIN_2, INPUT_PULLUP);
     initDisplay();
     initSensor();
     testWiFi();
     syncClock();
+    setupWiFi();
 }
 
 void loop()
 {
     currentState = digitalRead(BUTTON_PIN);
+    currentState2 = digitalRead(BUTTON_PIN_2);
 
     if(lastState == LOW && currentState == HIGH)
     {
@@ -49,5 +56,17 @@ void loop()
         }
     };
 
+    if(lastState2 == LOW && currentState2 == HIGH)
+    {
+        drawScreen_1();
+        // weather.begin();
+        // if(weather.update())
+        // {
+        //     showWeather(&weather);
+        // }
+        // weather.idle();
+    }
+
     lastState = currentState;
+    lastState2 = currentState2;
 }
